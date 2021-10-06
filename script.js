a86d017cf0fda7e0954ff005d78d0d737ba89232
Marco Jost <marco.jost@live.de>
2021-10-06 15:33:06 +0200

    TypeScript Katastrophe xD

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+// 1 x ...
+// var i = 1;
+// var msg = '';
+// while (i < 11) {
+//     msg = i + ' x 1 = ' + (i * 1) ;
+//     i++;
+//     console.log(msg);
+// document.write(msg);
+// }
+// // 2 x ...
+// var i = 1;
+// var msg = '';
+// while (i < 11) {
+//     msg = i + ' x 2 = ' + (i * 2) ;
+//     i++;
+//     console.log(msg);
+// document.write(msg);
+// }
+// // 3 x ...
+// var i = 1;
+// var msg = '';
+// while (i < 11) {
+//     msg = i + ' x 3 = ' + (i * 3);
+//     i++;
+//     console.log(msg);
+//     // document.write(msg);
+// }
+// // 4 x ...
+// var i = 1;
+// var msg = '';
+// while (i < 11) {
+//     msg = i + ' x 4 = ' + (i * 4) ;
+//     i++;
+//     console.log(msg);
+//     // document.write(msg);
+// }
+// 5 x ...
+// 6 x ...
+// 7 x ...
+// 8 x ...
+// 9 x ...
+// 10 x ...
+let msg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 1; i < 11; i++) {
+    msg[i] + ' x 2 = ' + (i * 2);
+    i++;
+    //    msg [i] + ' x 3 = ' + (i * 3) ; i++;
+    //    msg [i] + ' x 4 = ' + (i * 4) ; i++;
+    //    msg [i] + ' x 5 = ' + (i * 5) ; i++;
+    //    msg [i] + ' x 6 = ' + (i * 6) ; i++;
+    console.log(i);
+}
